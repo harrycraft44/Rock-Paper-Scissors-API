83d3c42a70db247cfaf704a6d9406e7ebd08f715
BuildTools <harrycraft11@outlook.com>
2021-12-14 18:35:04 +0000

    added images

--- index.js
+++ index.js
@@ -23,7 +23,47 @@ app.get('/', (req, res) => {
 
     var BotOptions = ["rock", "paper", "scissors"]
     var Bot = BotOptions[Random(0, 3)]
+    
+res.header('Content-Type', 'text/html')
+res.write("<h1> you     | vs |      ai</h1><br>")
+switch (rps){
+    case "scissors":
+        res.write(`<html>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Pair_of_scissors_with_black_handle%2C_2015-06-07.jpg/800px-Pair_of_scissors_with_black_handle%2C_2015-06-07.jpg"style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
+    case "rock":
+        res.write(`<html>
+<img src="https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8="style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
+    case "paper":
+        res.write(`<html>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Paper_450x450.jpg/220px-Paper_450x450.jpg"style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
 
+}
+res.write("vs")
+switch (Bot){
+    case "scissors":
+        res.write(`<html>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Pair_of_scissors_with_black_handle%2C_2015-06-07.jpg/800px-Pair_of_scissors_with_black_handle%2C_2015-06-07.jpg"style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
+    case "rock":
+        res.write(`<html>
+<img src="https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8="style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
+    case "paper":
+        res.write(`<html>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Paper_450x450.jpg/220px-Paper_450x450.jpg"style="width:130px; height: auto;" alt="scissors">
+</html>`)
+    break
+
+}
+res.write("<br>")
     // Draw
     if (Bot == "rock" && rps == "rock") {
         res.write("Draw!")
@@ -56,6 +96,8 @@ app.get('/', (req, res) => {
     else if (Bot == "scissors" && rps == "paper") {
         res.write("Lose!")
     }
+
+
     res.end()
     
 });app.listen(80)
